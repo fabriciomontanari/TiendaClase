77ebdd16eb6ca169c0b590cb98a81c193b4b1f4d
Fabri Montanari <136993786+fabriciomontanari@users.noreply.github.com>
2023-06-18 12:16:27 -0600

    Semana 2

---
+++ TiendaClase/src/main/java/com/TiendaClase/TiendaClaseApplication.java
@@ -0,0 +1,13 @@
+package com.TiendaClase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaClaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaClaseApplication.class, args);
+	}
+
+}
---
+++ TiendaClase/src/test/java/com/TiendaClase/TiendaClaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiendaClase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaClaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
